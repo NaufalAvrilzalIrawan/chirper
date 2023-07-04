992c0789c0ba4681e9181fd881345a91438dce7e
TNVR GAMES <naufalavir@gmail.com>
2023-07-04 11:17:05 +0700

    Menambah nama pada halaman dashboard dan mencoba memakai mailtrap

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Selamat Datang Kembali di Chirp!") }}
+                    {{ __("Selamat Datang Kembali di Chirp, " . Auth::user()->name ."!") }}
                 </div>
                 <img src="https://th.bing.com/th/id/R.26ff532f3d9f3661834014354af3958d?rik=HcLvz5o0TGO5%2fw&riu=http%3a%2f%2fimg13.deviantart.net%2fbb55%2fi%2f2014%2f207%2f3%2f8%2fsinh__the_slumbering_dragon_by_themefinland-d7sdgn8.jpg&ehk=fTSXyCVSTG2S3sLaAzpncZhLHRS6jhxZxCNVGLFmgUI%3d&risl=&pid=ImgRaw&r=0">
             </div>
